26721a74462fde644a82747efa6b1ad1ab5dc684
Saran313 <108916503+Saran313@users.noreply.github.com>
2023-03-31 17:01:15 +0530

    Update App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import World from './Component/World';
+
+
+function App() {
+  return (
+    <div className="App">
+      This Our Project
+<World/>
+    </div>
+  );
+}
+
+export default App;
